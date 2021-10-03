a803d0842d4598c0e6a25978ab2219ed3fd0932e
Pav <pav0107@gmail.com>
2021-10-03 14:57:14 +0100

    identify if website has invalid characters

--- lib/httpReport.rb
+++ lib/httpReport.rb
@@ -3,3 +3,12 @@ def start_program
   input = STDIN.gets.chomp
   @input_array = input.split("\\n")
 end
+
+def invalid_url_chars(website)
+  @invalid_url_chars = false
+  valid_characters_array = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~:/?#[]@!$&'()*+,;=".split("")
+  website.split("").each do |char|
+    @invalid_url_chars = true if !valid_characters_array.include?(char)
+  end
+  @invalid_url_chars
+end
